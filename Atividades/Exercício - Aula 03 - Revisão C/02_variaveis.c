894ff216bf0d2c47501940d808149068cfa448eb
Hendrick2301 <101660862+Hendrick2301@users.noreply.github.com>
2022-03-15 13:51:09 -0400

    Update 02_variaveis.c

--- Atividades/Exercício - Aula 03 - Revisão C/02_variaveis.c
+++ Atividades/Exercício - Aula 03 - Revisão C/02_variaveis.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void Biscoito(int* x,int y){
-    *x = y;
+void Pizza(int* w,int z){
+    *w = z;
 }
 
 void main(){
-    int a=0, b=0, c=0, d=0;
-    int *pa = &a, *pb = &b, *pc = &c, *pd = &d;
-    printf("\na = %d, b = %d c, = %d e d = %d\n\n",a,b,c,d);
-    Biscoito(pa, 7);
-    Biscoito(pb, 13);
-    Biscoito(pc, 27);
-    Biscoito(pd, 37);
-    printf("a = %d,b = %d c =%d d = %d\n\n",a,b,c,d);
+    int v=0, b=0, n=0, m=0;
+    int *pv = &v, *pb = &b, *pn = &n, *pm = &m;
+    printf("\nv = %m, b = %m n, = %m e m = %m\n\n",v,b,n,m);
+    Pizza(pv, 9);
+    Pizza(pb, 17);
+    Pizza(pn, 23);
+    Pizza(pmm, 39);
+    printf("v = %m,b = %m n =%m m = %m\n\n",v,b,n,m);
 }
